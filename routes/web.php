b03bd9c700fde19e0e83da2b6e1ad3e14809f1d4
aqilaauliah <19241552@bsi.ac.id>
2026-01-13 11:46:44 +0700

    memperbaiki halaman utama

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('welcome');
+    return view('welcome');
+});
+
+Route::get('/welcome', function () {
+    return view('welcome');
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
